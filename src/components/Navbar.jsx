9043c7bedf65fdd624d87fdf3fabd4ecd452cf1c
Safi <Nurzai.safi@gmail.com>
2022-07-06 13:50:04 +0430

    Navbar: slider styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import '../Style/navbar.css'
-import {Fragment} from 'react'
+import {Fragment, useState} from 'react'
 import { useLocation} from 'react-router-dom'
 import PEHOLogo from "../assets/PNG/PEHOLogo_White.png";
 import PEHOLogoBlack from "../assets/PNG/PEHOLogo_Orignal.png";
 const Navbar = () => {
   const location = useLocation()
+  const [showMenu, setShowMenu] = useState(false)
   const PathMatchRoute = (route) => {
     if (route === location.pathname) {
       return true;
@@ -40,13 +41,26 @@ const Navbar = () => {
           <div className="mobile-navlogo-box">
             <img src={PEHOLogo} alt="" />
           </div>
-          <div className="burger-menu-box">
+          <div className="burger-menu-box" onClick={() => setShowMenu(!showMenu)}>
             <div className="burger-bar"></div>
             <div className="burger-bar"></div>
             <div className="burger-bar"></div>
           </div>
         </div>
       </div>
+        {/* Mobile Menu Slider */}
+        {showMenu ?
+        <div className='mobile-slideMenu-box'>
+          <div className="slideMenu-link">
+            <a href="/">Home</a>
+          </div>
+          <div className="slideMenu-link">
+            <a href="/about-us">About us</a>
+          </div>
+          <div className="slideMenu-link">
+            <a href="/contact-us">Contact us</a>
+          </div>
+        </div> : null}
     </Fragment>
   )
 }
