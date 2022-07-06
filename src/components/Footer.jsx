27aa6c9c148b85074b538eefc4d96f6bc0e7a85b
Safi <Nurzai.safi@gmail.com>
2022-07-06 14:00:43 +0430

    Footer: styling and file start

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import {Fragment} from 'react'
+
+const Footer = () => {
+  return (
+    <Fragment>
+      <div className='footer-container'>
+
+      </div>
+    </Fragment>
+  )
+}
+
+export default Footer
