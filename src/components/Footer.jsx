7568c2e8c9e11c4114bcbae659dbfbe140cf8bf9
Safi <Nurzai.safi@gmail.com>
2022-07-06 14:32:51 +0430

    Footer: logo section done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import '../Style/footer.css'
 import { Fragment } from "react";
 import PEHOLogoWhite from "../assets/PNG/PEHOLogo_White.png";
 import facebookIcon from "../assets/SMicon/facebookIcon.png";
@@ -9,12 +10,12 @@ import youtubeIcon from "../assets/SMicon/youtubeIcon.png";
 const Footer = () => {
   return (
     <Fragment>
-      <div className="footer-container">
-        
+      <div className="footer-container bg-primary mt-40">
+
         <div className="footer-logo-box">
           <img src={PEHOLogoWhite} alt="Footer Logo" />
-          <h2>CONTACT WITH US</h2>
-          <div className="footer-socialMediaIcons-box">
+          <h2 className='text-xl'>CONTACT WITH US</h2>
+          <div className="footer-socialMediaIcons-box flex">
             <img src={facebookIcon} alt="Facebook icon" />
             <img src={twitterIcon} alt="Twitter icon" />
             <img src={instagramIcon} alt="Instagram icon" />
@@ -23,13 +24,13 @@ const Footer = () => {
           </div>
         </div>
         {/* Support Link */}
-        <div className="support-links-box">
+        <div className="support-links-box flex flex-col">
           <a href="/contact-us">Message us</a>
           <a href="/about-us">Know more about us</a>
           <a href="/about-us">Our team</a>
         </div>
         {/* Support Link */}
-        <div className="support-links-box2">
+        <div className="support-links-box2 flex flex-col">
           <a href="/empower-us">Empower women</a>
           <a href="/empower-us">Empower Health</a>
         </div>
