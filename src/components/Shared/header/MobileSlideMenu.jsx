16d72de7359bad05a9a1ee1ac2943e391cfbf07f
Safi <Nurzai.safi@gmail.com>
2022-07-14 16:25:42 +0430

    Navbar issues solved

--- src/components/Shared/header/MobileSlideMenu.jsx
+++ src/components/Shared/header/MobileSlideMenu.jsx
@@ -1,18 +1,18 @@
 import '../../../Style/Navbar/mobileMenuSlider.css'
 import { Link } from "react-router-dom"
 
-const MobileSlideMenu = () => {
+const MobileSlideMenu = ({onClick}) => {
   return (
     <>
       <div className='mobile-slideMenu-box fixed bg-bg-primary'>
           <div className="slideMenu-link text-xl">
-            <Link href="/">Home</Link>
+            <Link to="/">Home</Link>
           </div>
           <div className="slideMenu-link text-xl">
-            <Link href="/about-us">About us</Link>
+            <Link to="/about-us">About us</Link>
           </div>
           <div className="slideMenu-link text-xl">
-            <Link href="/contact-us">Contact us</Link>
+            <Link to="/contact-us">Contact us</Link>
           </div>
         </div>
     </>
