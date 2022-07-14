e59dc678b8b0206836d66b0b460ccc6bde8647b7
Safi <Nurzai.safi@gmail.com>
2022-07-14 13:40:01 +0430

    Navbar for mobile and desktop done

--- src/components/Shared/header/MobileNavbar.jsx
+++ src/components/Shared/header/MobileNavbar.jsx
@@ -1,18 +1,37 @@
-// import React from "react";
+import "../../../Style/navbar.css";
+import { useState} from 'react'
+const [showMenu, setShowMenu] = useState(false)
+import { Link, useLocation } from "react-router-dom";
+import PEHOLogo from "../../../assets/PNG/PEHOLogo_White.png";
 
-// const MobileNavbar = () => {
-//   return (
-//     <div className="mobile-navbar-box bg-primary md:hidden">
-//       <div className="mobile-navlogo-box">
-//         <img src={PEHOLogo} alt="" />
-//       </div>
-//       <div className="burger-menu-box" onClick={() => setShowMenu(!showMenu)}>
-//         <div className="burger-bar"></div>
-//         <div className="burger-bar"></div>
-//         <div className="burger-bar"></div>
-//       </div>
-//     </div>
-//   );
-// };
+const MobileNavbar = () => {
+  const location = useLocation();
+  return (
+    <>
+      <div className="mobile-navbar-box bg-primary md:hidden">
+        <div className="mobile-navlogo-box">
+          <img src={PEHOLogo} alt="" />
+        </div>
+        <div className="burger-menu-box" onClick={() => setShowMenu(!showMenu)}>
+          <div className="burger-bar"></div>
+          <div className="burger-bar"></div>
+          <div className="burger-bar"></div>
+        </div>
+      </div>
+      {showMenu ?
+        <div className='mobile-slideMenu-box fixed'>
+          <div className="slideMenu-link text-xl">
+            <Link href="/">Home</Link>
+          </div>
+          <div className="slideMenu-link text-xl">
+            <Link href="/about-us">About us</Link>
+          </div>
+          <div className="slideMenu-link text-xl">
+            <Link href="/contact-us">Contact us</Link>
+          </div>
+        </div> : null}
+    </>
+  );
+};
 
-// export default MobileNavbar;
+export default MobileNavbar;
--- src/components/Shared/header/Navbar.jsx
+++ src/components/Shared/header/Navbar.jsx
@@ -1,67 +1,10 @@
-import '../../../Style/navbar.css'
-import {Fragment, useState} from 'react'
-import { Link, useLocation} from 'react-router-dom'
-import PEHOLogo from "../../../assets/PNG/PEHOLogo_White.png";
-import PEHOLogoBlack from "../../../assets/PNG/PEHOLogo_Orignal.png";
+import { Fragment} from "react";
 const Navbar = () => {
-  const location = useLocation()
-  const [showMenu, setShowMenu] = useState(false)
-  const PathMatchRoute = (route) => {
-    if (route === location.pathname) {
-      return true;
-    }
-  }
   return (
     <Fragment>
-      <div className='container navbar-container'>
-        <div className={PathMatchRoute('/')? "navbar-box bg-primary h-28": 'navbar-box bg-white h-28'}>
-          <div className="navlogo-box">
-            <img src={PathMatchRoute('/')?PEHOLogo:PEHOLogoBlack} alt="Navbar Logo" />
-          </div>
-          <div className="navLinks-box">
-            <Link to={'/'} >
-            <span className={PathMatchRoute('/')? 'text-white active-link':'text-black'}>Home</span>
-            </Link>
-            <Link to={'/about-us'}>
-            <span className={PathMatchRoute('/')? 'text-white':'text-black'}>
-              <span className={PathMatchRoute('/about-us')? 'active-link': ''}>About us</span>
-            </span>
-            </Link>
-            <Link to={'/contact-us'}>
-              <span className={PathMatchRoute('/')? 'text-white':'text-black'}>
-                <span className={PathMatchRoute('/contact-us')? 'active-link': ''}>Contact us</span>
-              </span>
-            </Link>
-          </div>
-        </div>
-
-        {/* Mobile Version */}
-        <div className="mobile-navbar-box bg-primary">
-          <div className="mobile-navlogo-box">
-            <img src={PEHOLogo} alt="" />
-          </div>
-          <div className="burger-menu-box" onClick={() => setShowMenu(!showMenu)}>
-            <div className="burger-bar"></div>
-            <div className="burger-bar"></div>
-            <div className="burger-bar"></div>
-          </div>
-        </div>
-      </div>
-        {/* Mobile Menu Slider */}
-        {showMenu ?
-        <div className='mobile-slideMenu-box fixed'>
-          <div className="slideMenu-link text-xl">
-            <a href="/">Home</a>
-          </div>
-          <div className="slideMenu-link text-xl">
-            <a href="/about-us">About us</a>
-          </div>
-          <div className="slideMenu-link text-xl">
-            <a href="/contact-us">Contact us</a>
-          </div>
-        </div> : null}
+      
     </Fragment>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
